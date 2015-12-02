533866884de0af39b3ed624379aa359416858758
Aaron Filson <aaronfilson@gmail.com>
2015-12-01 19:03:52 -0800

    About section transitions in and out.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
 // clone the marked up html article in the html and use it for the further posts
 blog.makeNewIndex();
 blog.toHTML();
+$( Util.atLoadFunction );
+$('nav').on('click', function(event) {
+  event.preventDefault();
+  if(event.target.className == 'aboutMeNav') {
+    $('.aboutme').show();
+    $('article').hide();
+  }
+
+  if (event.target.className == 'home') {
+    Util.makeItReady();
+  }
+
+}); //register clicks on nav
--- scripts/util.js
+++ scripts/util.js
@@ -1,16 +1,23 @@
 var Util = {};
 
-Util.makeItReady = function () {
+Util.makeItReady = function() {
   //hide the lower p, hide about and prep filters
+  $('.aboutme').hide();
+  $('article').show();
+  $('.body p:not(:first-child)').hide();
 };
 
-Util.handleNavClick = function (e) {
+Util.handleNavClick = function(eventarg) {
   //handle clicks to the nav
-  
+
 };
 
 Util.atLoadFunction = function() {
   //We are now ready
   Util.makeItReady(); //hide articles and prep filters
-  $('header').on('click', 'nav', Util.handleNavClick(e)); //register clicks on nav
+};
+
+Util.revealReadOn = function(context) {
+  //unhide the article paragraphs so the reader has access
+
 };
