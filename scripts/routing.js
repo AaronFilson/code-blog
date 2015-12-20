046f02f0a1ee116b14a03c3b5088e9b8875aae01
Aaron Filson <aaronfilson@gmail.com>
2015-12-19 19:16:28 -0800

    Highlighter working better in new.html.

--- scripts/routing.js
+++ scripts/routing.js
@@ -4,6 +4,9 @@ page('/about', aboutFunction);
 page('/indexs', defaultToIndex);
 page('/indexed', defaultToIndex);
 
+page('/category/:category', articlesController.category, articlesController.show);
+page('/category/:author', articlesController.author, articlesController.show);
+
 page('/', defaultToIndex);
 page('/*', defaultToIndex);
 
